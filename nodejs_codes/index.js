5b2070dd4a0882b9632a506c35efef29efdf0b1c
Aman Bisht <amanvista@gmail.com>
2022-06-22 12:58:26 +0530

    progress bar added

--- nodejs_codes/index.js
+++ nodejs_codes/index.js
@@ -1,1 +1,19 @@
 console.log("hello", process.env.USER_ID)
+const doSomething = () => console.log('test');
+const measureDoingSomething = () => {
+  console.time('doSomething()');
+  // do something, and measure the time it takes
+  doSomething();
+  console.timeEnd('doSomething()');
+};
+measureDoingSomething();
+
+const ProgressBar = require('progress');
+
+const bar = new ProgressBar(':bar', { total: 10 });
+const timer = setInterval(() => {
+  bar.tick();
+  if (bar.complete) {
+    clearInterval(timer);
+  }
+}, 100);
